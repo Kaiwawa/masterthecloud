2e61b774529895e582334d12c84cf81fb9b84a63
Kaiwawa <rafael_c_baumann@outlook.com.br>
2026-04-30 23:35:00 -0300

    v0.9.8.24

--- assets/js/version.js
+++ assets/js/version.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v0.9.3';
+const APP_VERSION = 'v0.9.8.24';
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.app-version').forEach(el => {
